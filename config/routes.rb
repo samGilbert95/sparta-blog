2a96e89ba081da46b463c6b7bee29daf3a4e2bba
Tech-A65 <tech-a65@Tech-A56s-MacBook-Air.local>
2018-10-03 14:01:35 +0100

    Controller Commit 4: Created Comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static#landing'
   resources :users
-  resources :blogs
+  resources :blogs do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

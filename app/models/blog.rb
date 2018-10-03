d25b2622ebdb40218c685555472b0ae08e0964a5
Tech-A65 <tech-a65@Tech-A56s-MacBook-Air.local>
2018-10-04 00:01:37 +0100

    Styling Commit 3: Added styling to each page, added images to blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,7 @@
 class Blog < ApplicationRecord
   belongs_to :user
   has_many :comments
-  validates :title, :presence => true , :length => {:in => 1..5}
+  validates :title, :presence => true , :length => {:in => 1..100}
   validates :body, :presence => true
   validates :image, :presence => true
 end

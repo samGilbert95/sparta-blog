9a597786c962b9d546ff37edf7f7d692d7fc0cbd
Tech-A65 <tech-a65@Tech-A56s-MacBook-Air.local>
2018-10-04 09:18:18 +0100

    Edited Based on Feedback

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,32 +1,24 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
-  # GET /comments
-  # GET /comments.json
   def index
     @blog = Blog.find(params[:blog_id])
     @comments = Comment.all
   end
 
-  # GET /comments/1
-  # GET /comments/1.json
   def show
     @blog = Blog.find(params[:blog_id])
   end
 
-  # GET /comments/new
   def new
     @blog = Blog.find(params[:blog_id])
     @comment = Comment.new
   end
 
-  # GET /comments/1/edit
   def edit
     @blog = Blog.find(params[:blog_id])
   end
 
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
     @blog = Blog.find(params[:blog_id])
@@ -42,8 +34,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
     respond_to do |format|
       if @comment.update(comment_params)
@@ -56,8 +46,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment.destroy
     respond_to do |format|
@@ -67,12 +55,10 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
       params.require(:comment).permit(:blog_id, :user_id, :body)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments
   validates :first_name, :presence => true, format: {:with => /[a-zA-Z\-\_20%]+/}
   validates :last_name, :presence => true, format: {:with => /[a-zA-Z\-\_20%]+/}
-  validates :dob, :presence => true, numericality: true
+  validates :dob, :presence => true
 
   def full_name
     "#{first_name} #{last_name}"
